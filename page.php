f07ab56ae6c52a23e0356fbc7bc82d747e0c6b32
Kristjan Frederiksen <kristjanf@gmail.com>
2017-05-29 21:35:26 +0200

    Added background image

--- page.php
+++ page.php
@@ -1,16 +1,17 @@
 <?php get_header(); ?>
 
-<div class="row">
-    <div class="col">
         <?php if (have_posts()) : ?>
 
             <?php while (have_posts()): the_post(); ?>
 
-                <div class="container pt-md-5 background-temp">
-                    <div class="header pt-md-5">
+            <div class="background_image col-12"
+                <?php if (get_field('background_image')) { echo 'style="background-image: url('
+                    . get_field('background_image') . '); background-size: cover;"'; } ?>>
+                <div class="container pt-5">
+                    <div class="header pt-5">
                         <h1 class="headline col-12 col-lg-8"><?php the_field('header'); ?></h1>
                     </div>
-                    <div class="header_teaxtarea col-12 col-lg-6">
+                    <div class="header_teaxtarea col-12 col-lg-7">
                         <p><?php the_field('header_subtext'); ?></p>
                     </div>
                     <div class="courses_button col">
@@ -22,18 +23,19 @@
                         </a>
                     </div>
                 </div>
+            </div>
 
                 <div class="white_background">
                     <div class="container d-lg-flex">
-                        <div class="welcome col-12 col-lg-6 pl-lg-0 pt-5" id="welcome">
+                        <div class="welcome col-12 col-lg-6 pl-lg-0" id="welcome">
                             <p><?php the_field('welcome'); ?></p>
                             <div class="welcome_text ml-lg-0">
                                 <p><?php the_field('welcome_subtext'); ?></p>
                             </div>
                         </div>
-                        <div class="welcome col-12 col-lg-6 ml-lg-4 pt-sm-5">
+                        <div class="why_yrgo col-12 col-lg-6 ml-lg-4">
                           <p><?php the_field('why_yrgo'); ?></p>
-                            <div class="embed-responsive embed-responsive-16by9">
+                            <div class="yrgo_video embed-responsive embed-responsive-16by9">
                               <?php the_field('welcome_video'); ?>
                             </div>
                         </div>
